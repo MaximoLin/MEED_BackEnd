594ddfad810a6133ade83e5a58ebc0730501ae25
Maximo Lin <linmaximomo@gmail.com>
2021-08-22 23:56:10 -0300

    update MEED_BDD
    Hice la conexion con la BDD, cree la carpeta y el archivo, los subo al repositorio.

---
+++ MEED_BDD/conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$clave = "";
+$BDD = "meed_test";
+
+$conexion = new mysqli($host,$user,$clave,$BDD);
+
+if($conexion->connect_error)
+{
+   die("conexion fallida: " . $conexion->connect_error); 
+    echo "conexion fallida " . mysql_error();
+}
+else
+{
+    echo "Conectado";
+}
+
+?>
